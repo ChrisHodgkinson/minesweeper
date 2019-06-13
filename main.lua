cec077b25cdc19a9e76c69b0ae41c2aaa5eaa502
Chris Hodgkinson <chris@aquietspace.org>
2019-06-13 09:58:16 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -27,14 +27,26 @@ local function placeMines ()
 
 end
 
-local function createBoard ()
-  --for f = 
-end
-
 local function zoneClicked( event )
   local thisZone = event.target
   local phase = event.phase
 end
+
+local function createBoard ()
+  local vOff = 1
+  for r = 1, boardHeight do
+    local hOff = 1
+    board[r]={}
+    for c = 1, boardWidth do 
+      board[r][c] = display.newRect(((32*c)+hOff), ((32*r)+vOff), 31, 31 )
+      board[r][c]:setFillColor(.3, .3, .9, 1)
+      board[r][c].strokeWidth = 1 ; board[r][c]:setStrokeColor(0,0,0)
+      hOff = hOff + 1
+    end
+    vOff = vOff + 1
+  end
+end
+createBoard()
 Runtime:addEventListener ( "key", keyListener )
 
 
