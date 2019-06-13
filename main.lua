46c8729b450e667805bd5d5a4f4f66e6274fe46e
Chris Hodgkinson <chris@aquietspace.org>
2019-06-13 08:40:53 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,12 +7,13 @@ local board = {}
 local boardHeight = 15
 local boardWidth  = 15
 local totalMines  = 30
-local cmdDown     = false
+local ctrlDown     = false
 
 
 local function keyListener ( event )
-  local e = event.phase
-  local key = event.key
+  local phase = event.phase
+  local key = event.keyName
+  ctrlDown = (key == "leftControl" or key == "rightControl") and phase == "down" or false
 end
 
 local function placeNumbers ()
@@ -30,7 +31,8 @@ end
 local function zoneClicked( event )
   local thisZone = event.target
   local phase = event.phase
-
 end
+Runtime:addEventListener ( "key", keyListener )
+
 
 -- EOF
