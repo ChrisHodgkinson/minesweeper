2ca24e74eaf611dcfbf4535e73a7213b847162b8
Chris Hodgkinson <chris@aquietspace.org>
2019-06-13 07:32:26 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,8 +7,30 @@ local board = {}
 local boardHeight = 15
 local boardWidth  = 15
 local totalMines  = 30
+local cmdDown     = false
 
 
+local function keyListener ( event )
+  local e = event.phase
+  local key = event.key
+end
 
+local function placeNumbers ()
+
+end
+
+local function placeMines ()
+
+end
+
+local function createBoard ()
+
+end
+
+local function zoneClicked( event )
+  local thisZone = event.target
+  local phase = event.phase
+
+end
 
 -- EOF
